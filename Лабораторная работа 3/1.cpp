acb39920986c7c4d76c0b8933d15f156c962bf88
ulkalinina <ulkalinina04@mail.ru>
2024-12-18 22:24:41 +0300

    Add files via upload

---
+++ Лабораторная работа 3/1.cpp
@@ -0,0 +1,127 @@
+﻿#include <iostream>
+#include <vector>
+#include <memory>
+#include <string>
+
+using namespace std;
+
+// Базовый класс "Detail" (деталь)
+class Detail {
+protected:
+    // Конструктор protected, чтобы запретить явное создание объектов
+    Detail() = default;
+
+public:
+    // Виртуальный деструктор для корректного удаления производных объектов
+    virtual ~Detail() = default;
+
+    // виртуальный метод для полиморфного поведения
+    virtual void show() const = 0;
+
+    // Дружественная функция для создания объектов
+    template <typename T, typename... Args>
+    friend shared_ptr<T> make_shared_instance(Args&&... args);
+};
+
+// Производный класс "Assembly" (сборка), представляющий группу деталей
+class Assembly : public Detail {
+protected:
+    vector<shared_ptr<Detail>> parts;
+
+    // Конструктор protected, чтобы запретить явное создание объектов
+    Assembly() = default;
+
+public:
+    // Метод для добавления деталей в сборку
+    void add_part(shared_ptr<Detail> part) {
+        parts.push_back(part);
+    }
+
+    // Переопределение метода show()
+    void show() const override {
+        cout << "Сборка, состоящая из следующих частей:\n";
+        for (const auto& part : parts) {
+            cout << "  - ";
+            part->show();
+        }
+    }
+};
+
+// Производный класс "Screw" (винт) как пример отдельной детали
+class Screw : public Detail {
+protected:
+    Screw() = default;
+
+public:
+    void show() const override {
+        cout << "Винт\n";
+    }
+};
+
+// Производный класс "Nut" (гайка) как пример отдельной детали
+class Nut : public Detail {
+protected:
+    Nut() = default;
+
+public:
+    void show() const override {
+        cout << "Гайка\n";
+    }
+};
+
+// Производный класс "Bolt" (болт) как пример отдельной детали
+class Bolt : public Detail {
+protected:
+    Bolt() = default;
+
+public:
+    void show() const override {
+        cout << "Болт\n";
+    }
+};
+
+// Шаблонная дружественная функция для создания объектов
+template <typename T, typename... Args>
+shared_ptr<T> make_shared_instance(Args&&... args) {
+    struct make_shared_enabler : public T {
+        make_shared_enabler(Args&&... args) : T(forward<Args>(args)...) {}
+    };
+    return make_shared<make_shared_enabler>(forward<Args>(args)...);
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    // Вектор для хранения указателей на объекты типа Detail
+    vector<shared_ptr<Detail>> details;
+
+    // Создаем несколько деталей
+    auto screw = make_shared_instance<Screw>();
+    auto nut = make_shared_instance<Nut>();
+    auto bolt = make_shared_instance<Bolt>();
+
+    // Добавляем детали в список для вывода
+    details.push_back(screw);
+    details.push_back(nut);
+    details.push_back(bolt);
+
+    // Создаем сборку, состоящую из винта, гайки и болта
+    auto assembly = make_shared_instance<Assembly>();
+    assembly->add_part(screw);
+    assembly->add_part(nut);
+    assembly->add_part(bolt);
+
+    // Добавляем сборку в список
+    details.push_back(assembly);
+
+
+    cout << "Детали:\n";
+
+    // Выводим информацию о каждом объекте
+    for (const auto& detail : details) {
+        detail->show();
+        cout << endl;
+    }
+
+    return 0;
+}
---
+++ Лабораторная работа 3/2z.exe
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <vector>
+#include <memory>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class Base {
+public:
+    virtual ~Base() {}
+
+    virtual void show() const {
+        cout << "I'm a Base object." << endl;
+    }
+};
+
+class Derived : public Base {
+public:
+    void show() const override {
+        cout << "I'm a Derived object." << endl;
+    }
+};
+
+class Storage {
+private:
+    vector<shared_ptr<Base>> storage; // Используем shared_ptr для управления памятью
+
+public:
+    void add(shared_ptr<Base> obj) {
+        storage.push_back(obj); // Добавляем объект в хранилище
+    }
+
+    void displayAll() const {
+        for (const auto& obj : storage) {
+            obj->show(); // Вызов метода show для каждого объекта
+        }
+    }
+
+    void clearStorage() {
+        storage.clear(); // Очищаем хранилище, освобождая ресурсы
+    }
+};
+
+int main() {
+    srand(static_cast<unsigned int>(time(0))); // Инициализация генератора случайных чисел
+    Storage storage;
+
+    for (int i = 0; i < 10; i++) { // Генерируем 10 объектов
+        int randomNumber = rand();
+        if (randomNumber % 2 == 0) { // Четное число создает Base
+            shared_ptr<Base> basePtr = make_shared<Base>();
+            storage.add(basePtr);
+        }
+        else { // Нечетное число создает Derived
+            shared_ptr<Base> derivedPtr = make_shared<Derived>();
+            storage.add(derivedPtr);
+        }
+    }
+
+    // Показать все объекты в хранилище
+    storage.displayAll();
+
+    // Очистить хранилище перед завершением программы
+    storage.clearStorage();
+
+    return 0; // Завершение программы
+}
